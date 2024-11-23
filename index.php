61f991a397994cac063bdde1e2e149dfcfb87936
rahmadde <teukurahmad120@gmail.com>
2024-11-23 09:57:04 +0700

    Membuat Tampilan Awal Aplikasi

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Manager</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="card col-6 mt-5 m-auto col-7 row">
+        <div class="card-header">
+            <h3 class="float-start">Employee Manager</h3>
+            <span class="float-end">
+                <a href="" class="btn btn-success">
+                    <font class="">Tambah Data</font>
+                </a>
+            </span>
+        </div>
+         <div class="card-body">
+            <div class="mb-2 mt-2">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID Karyawan</th>
+                            <th scope="col">Nama Kayawan</th>
+                            <th scope="col">Jabatan</th>
+                            <th scope="col">Tanggal Masuk</th>
+                            <th scope="col">Gaji</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"></th>
+                            <td scope="col"></td>
+                            <td scope="col"></td>
+                            <td scope="col"></td>
+                            <td scope="col"></td>
+                            <td scope="col">
+                                <a href="" class="btn btn-info btn-sm">
+                                    Edit
+                                </a>
+                                <a href="" class="btn btn-danger btn-sm">
+                                    Hapus
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

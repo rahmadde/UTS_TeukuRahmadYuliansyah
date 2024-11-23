002910509eeb2ea246f2421330d1838299312af3
rahmadde <teukurahmad120@gmail.com>
2024-11-23 09:59:46 +0700

    Membuat Koneksi PHP

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "uts_aplikasi_teukurahmadyuliansyah";
+
+    $koneksi = mysqli_connect($host, $username, $password, $database);
+?>
